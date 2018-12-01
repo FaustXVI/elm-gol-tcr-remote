f138b8bbedb1950f49a4045027f0b7b822306597
Patrick GIRY <patrickgiry@gmail.com>
2018-12-01 11:13:29 +0100

    merge test

--- tests/MainSpecs.elm
+++ tests/MainSpecs.elm
@@ -6,8 +6,19 @@ import Test exposing (..)
 import Main exposing (..)
 
 suite : Test
+<<<<<<< HEAD
 suite =
     describe "Game of life"
         [ test "should init a grid" <|
             \_ -> Expect.equal (init) [[]]
+=======
+suite = [
+    describe "A test"
+        [ test "should fail" <|
+            \_ -> Expect.equal 3 (add 2 1)
+        ],
+    describe "A second test"
+        [ test "should create a grid" <|
+            \_ -> Expect.equal [[]] (grid 0 0)
+>>>>>>> Add to initialize an empty grid
         ]
