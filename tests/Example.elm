adcfa51e080b4a9ee84dc4633c50cbdb5fe87a92
ToF <cthibaut@octo.com>
2018-12-01 10:39:34 +0100

    add function

--- tests/Example.elm
+++ tests/Example.elm
@@ -4,10 +4,10 @@ import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
 import Test exposing (..)
 
-
+add x y = x + y
 suite : Test
 suite =
     describe "A test"
         [ test "should fail" <|
-            \_ -> Expect.equal 3 3
+            \_ -> Expect.equal 3 (add 2 1)
         ]
