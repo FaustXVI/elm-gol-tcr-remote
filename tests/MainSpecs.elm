815fbf7e80471f5808f7fffb835027af90b9745c
FaustXVI <xavier.detant@gmail.com>
2018-12-01 10:55:07 +0100

    Add initialisation step

--- tests/MainSpecs.elm
+++ tests/MainSpecs.elm
@@ -1,13 +1,12 @@
 module MainSpecs exposing (..)
 
 import Expect exposing (Expectation)
-import Fuzz exposing (Fuzzer, int, list, string)
 import Test exposing (..)
+import Main exposing (..)
 
-add x y = x + y
 suite : Test
 suite =
-    describe "A test"
-        [ test "should fail" <|
-            \_ -> Expect.equal 3 (add 2 1)
+    describe "Game of life"
+        [ test "should init a grid" <|
+            \_ -> Expect.equal (init) [[]]
         ]
