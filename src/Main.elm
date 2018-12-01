1d5fc1029f768c13ad4c9267a14a03ec526d5c05
FaustXVI <xavier.detant@gmail.com>
2018-12-01 11:01:40 +0100

    Add view

--- src/Main.elm
+++ src/Main.elm
@@ -1,10 +1,14 @@
 module Main exposing (..)
-import Html exposing (text)
+import Html exposing (text, Html)
 
 type alias Model = List (List Bool)
 
 init : Model
 init = [[]]
 
-main =
+type Msg = NoOp
+
+view : Model -> Html Msg
+view _ =
     text "Hello, world!"
+
--- tests/MainSpecs.elm
+++ tests/MainSpecs.elm
@@ -3,10 +3,13 @@ module MainSpecs exposing (..)
 import Expect exposing (Expectation)
 import Test exposing (..)
 import Main exposing (..)
+import Html exposing (text)
 
 suite : Test
 suite =
     describe "Game of life"
         [ test "should init a grid" <|
             \_ -> Expect.equal (init) [[]]
+        , test "should show a grid" <|
+            \_ -> Expect.equal (view [[]]) (text "Hello, world!")
         ]
