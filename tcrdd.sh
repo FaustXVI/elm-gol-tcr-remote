c9fb617c7e2d1d5928c9c675086dd6e3bccc7899
ToF <cthibaut@octo.com>
2018-12-01 15:00:59 +0100

    commented and renamed things on tcrdd.sh

--- tcrdd.sh
+++ tcrdd.sh
@@ -6,13 +6,15 @@ function runTest() {
     ./node_modules/.bin/elm-test
 }
 
+# look for a new test in a + line in the diff
 function testJustAdded(){
     [[ ! -z `git diff HEAD | grep "^\+.*${TEST_KEYWORD}"` ]]
 }
 
 RED_REF=refs/isRed
 
-function commitRed() {
+# amend in case of test refactoring (last commit is red, same test)
+function commitOnRed() {
     git add . && \
     if lastCommitRed; then
         git commit --amend
@@ -22,7 +24,10 @@ function commitRed() {
     git update-ref ${RED_REF} HEAD
 }
 
-function commitGreen() {
+# amend if last commit was a test, with no change to the commit message
+# in case if last commit was green commit with empty message (/?\ should refactoring have commit messages?)
+# remove any RED tag reference
+function commitOnGreen() {
     git add . && \
     if lastCommitRed; then
         git commit --amend --no-edit
@@ -32,38 +37,45 @@ function commitGreen() {
     git update-ref -d ${RED_REF}
 }
 
+# finds a previous commit tagged Red, returns true if same as current work
 function lastCommitRed(){
     [[ `git describe --all ${RED_REF} 2>/dev/null` && -z `git diff ${RED_REF} HEAD 2>/dev/null` ]]
 }
 
+# revert to the last version that was committed
 function revert() {
     git reset --hard
     git clean -f
 }
 
+# pull branch if there's a remote change
+# rebase so that our commit will come *after* remote changes
 function pull(){
     if needsPull; then
         git pull --rebase
     fi
 }
 
+# detect remote changes (not actually fetching them)
 function needsPull(){
     [[ ! -z `git fetch --dry-run 2>&1` ]]
 }
 
+# if push required, launch tests then push
 function push() {
     if needsPush; then
         runTest && git push
     fi
 }
 
+# detect local changes wrt remote
 function needsPush(){
     [[ ! -z `git diff ${BRANCH} HEAD` ]]
 }
 
-
-KNOWN_AS_GREEN=false
-KNOWN_AS_RED=false
+# main
+ASSUMING_GREEN=false
+ASSUMING_RED=false
 
 while [[ $# -gt 0 ]]
 do
@@ -71,11 +83,11 @@ do
 
     case ${key} in
         -g|--green)
-            KNOWN_AS_GREEN=true
+            ASSUMING_GREEN=true
             shift
             ;;
         -r|--red)
-            KNOWN_AS_RED=true
+            ASSUMING_RED=true
             shift
             ;;
         *)
@@ -84,13 +96,13 @@ do
     esac
 done
 
-if ${KNOWN_AS_RED} || (! ${KNOWN_AS_GREEN} && testJustAdded)
+if ${ASSUMING_RED} || (! ${ASSUMING_GREEN} && testJustAdded)
 then
     echo red
-    runTest && revert || commitRed
+    runTest && revert || commitOnRed
     pull
 else
     echo green
-    runTest && commitGreen || revert
+    runTest && commitOnGreen || revert
     pull && push
 fi
